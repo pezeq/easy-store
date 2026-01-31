71cbe5742f0e8d2313426d5e74798f5f7c8bc8e5
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2026-01-31 19:57:46 -0300

    chore: revise error messages and import

--- backend/src/shared/utils/common.validation.ts
+++ backend/src/shared/utils/common.validation.ts
@@ -4,7 +4,7 @@ export const idSchema = z.object({
 	id: z.coerce
 		.number<number>("Params ID must be a number")
 		.int("Params ID must be an integer")
-		.positive("Params ID must be positive"),
+		.positive("Params ID must be greater than 0"),
 });
 
 export type idSchemaType = z.infer<typeof idSchema>;
@@ -13,13 +13,13 @@ export const paginationSchema = z.object({
 	limit: z.coerce
 		.number<number>("Limit must be a number")
 		.int("Limit must be an integer")
-		.positive("Limit must be positive")
+		.positive("Limit must be greater than 0")
 		.max(20, "Limit maximum value is 20")
 		.default(20),
 	offset: z.coerce
 		.number<number>("Offset must be a number")
 		.int("Offset must be an integer")
-		.min(0, "Offset must be 0 or higher")
+		.min(0, "Offset must be greater than 0")
 		.default(0),
 });
 
--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -1,4 +1,4 @@
-import type { ReqUser } from "@modules/auth/auth.types";
+import type { ReqUser } from "@modules/auth/auth.types.js";
 
 declare global {
 	namespace Express {
