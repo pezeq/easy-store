bf13bc2cdb51261479ad97747ecff501137cabb8
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2026-02-07 19:43:51 -0300

    feat: add getReqUser method to auth utils

--- backend/src/modules/auth/auth.utils.ts
+++ backend/src/modules/auth/auth.utils.ts
@@ -1,5 +1,7 @@
 import { SALT_ROUND, SECRET } from "@shared/config/config.js";
+import { AuthError } from "@shared/errors/appErrors.js";
 import bcrypt from "bcrypt";
+import type { Request } from "express";
 import jwt from "jsonwebtoken";
 
 export const generateToken = (id: number, username: string): string => {
@@ -22,4 +24,12 @@ export const checkPasswordMatches = async (
 
 export const hashPassword = async (password: string): Promise<string> => {
 	return await bcrypt.hash(password, SALT_ROUND);
+};
+
+export const getReqUser = (req: Request) => {
+	if (!req.user) {
+		throw new AuthError();
+	}
+
+	return req.user;
 };
